3d91e7ea28a85a4014123e73426a1add58f72dc9
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-03-11 11:21:52 -0800

    completed feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -30,15 +30,16 @@ var displayRepos = (repos, searchTerm) => {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    // create a container for each repo
-    var repoEl = document.createElement("div");
+    // create a link for each repo
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
 
-    // append to cpntainer
+    // append to container
     repoEl.appendChild(titleEl);
 
     var statusEl = document.createElement("span");
@@ -49,10 +50,10 @@ var displayRepos = (repos, searchTerm) => {
       statusEl.innerHTML =
         "<i class='fas fa-times status-icon icon-danger'></i>" +
         repos[i].open_issues_count +
-        " issuie(s)";
+        " issue(s)";
     } else {
       statusEl.innerHTML =
-        "<i class='fas fa-check-sqaure status-icon icon-success'></i>";
+        "<i class='fas fa-check-square status-icon icon-success'></i>";
     }
 
     // append to container
@@ -71,20 +72,20 @@ var getUserRepos = function (user) {
 
   // make a request to the url
   fetch(apiUrl)
-  .then(function (response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data, user);
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  })
-  .catch(function(error) {
-    // Notice this `.catch()` getting chained onto oothe end of the `.then()` method
-    alert("Unable to connect to GitHub");
-  });
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert("Unable to connect to GitHub");
+    });
 };
 
 getUserRepos();
